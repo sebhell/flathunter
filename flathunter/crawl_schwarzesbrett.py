a03eed1ed55c68dc109d53caca38334d76ca8070
Sebastian Hellmann <seb_hel@uni-bremen.de>
2019-09-05 23:55:46 +0200

    enable blacklist and disable link preview

--- flathunter/crawl_schwarzesbrett.py
+++ flathunter/crawl_schwarzesbrett.py
@@ -13,6 +13,7 @@ class CrawlSchwarzesBrettBremen:
     __log__ = logging.getLogger(__name__)
     URL_PATTERN = re.compile(r'https://schwarzesbrett\.bremen\.de')
     maxlen = 400
+    blacklist = []
 
     def __init__(self):
         logging.getLogger("requests").setLevel(logging.WARNING)
@@ -20,7 +21,13 @@ class CrawlSchwarzesBrettBremen:
     def get_results(self, search_url):
         self.__log__.debug("Got search URL %s" % search_url)
 
-        soup = self.get_page(search_url)
+        li = search_url.split('!')
+        if len(li) > 1:
+            self.blacklist = [s.strip() for s in li[1].split(',')]
+        else:
+            self.blacklist = []
+
+        soup = self.get_page(li[0])
 
         # get data from first page
         entries = self.extract_data(soup)
@@ -32,6 +39,9 @@ class CrawlSchwarzesBrettBremen:
     def get_page(self, search_url):
         cur_page = self.dump_html(search_url)
         return BeautifulSoup(cur_page, features='html.parser', parse_only=SoupStrainer(['ul', 'li']))
+
+    def is_interesting(self, text):
+        return not any(x.lower() in text.lower() for x in self.blacklist)
 
     def extract_data(self, soup):
 
@@ -47,15 +57,15 @@ class CrawlSchwarzesBrettBremen:
                 if date == cur_date:
                     link = li_tag.find_all('a', href=True)[0]['href']
                     details = self.fetch_details(base_url + link)
-
-                    offer = {
-                        'id': abs(hash(title)) % (10**8),
-                        'url': base_url + link,
-                        'title': title,
-                        'schwarzesbrett': details[:self.maxlen] + '...' if len(details) > self.maxlen else details,
-                        'address': ""
-                    }
-                    fetched_offers.append(offer)
+                    if self.is_interesting(title+details):
+                        offer = {
+                            'id': abs(hash(title)) % (10**8),
+                            'url': base_url + link,
+                            'title': title,
+                            'schwarzesbrett': details[:self.maxlen] + '...' if len(details) > self.maxlen else details,
+                            'address': ""
+                        }
+                        fetched_offers.append(offer)
 
         return fetched_offers
 
